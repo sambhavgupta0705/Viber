9fb10e0500a52f1b4aec3abf1728b931a671e43b
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-05-20 12:24:22 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const {chats}=require("./data/data");
-
+const dotenv=require('dotenv');
 const app = express();
+dotenv.config();
 
 app.get("/",(req,res)=>{
     res.send("API is running")
 })
 
-app.get("/api/",(req,res)=>{
+app.get("/api/chat",(req,res)=>{
     res.send(chats)
 })
-app.listen(5000,console.log("Port has started running on port 5000"));
+
+
+app.get("/api/chat/:id",(req,res)=>{
+    console.log(req.params.id);
+})
+
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(5000,console.log(`Port has started running on port ${PORT}`));
