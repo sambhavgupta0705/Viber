dfa3846c1df06dc6e3e4320d13f81d7068fbc6e0
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-05-21 17:37:28 +0530

    authentication

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,16 @@ const {chats}=require("./data/data");
 const dotenv=require('dotenv');
 const connectDB = require('./config/db');
 const app = express();
-
-
+const userRoutes=require('./routes/userRoutes');
 dotenv.config();
 connectDB();
-
-
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("API is running")
 })
 
-app.get("/api/chat",(req,res)=>{
-    res.send(chats)
-})
-
-
-app.get("/api/chat/:id",(req,res)=>{
-    console.log(req.params.id);
-})
-
+app.use('/api/user',userRoutes)
 
 const PORT=process.env.PORT || 5000;
 
