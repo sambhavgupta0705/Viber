e905689a59a54784d9fb6f591b685583c8a54df3
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-05-21 15:21:50 +0530

    integrated mongodb

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,28 @@
-import { Container,Center, Box, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  Center,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
-const HomePage = () => {
+function Homepage() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
   return (
     <Container maxW="xl" centerContent>
       <Box
@@ -14,11 +35,28 @@ const HomePage = () => {
         borderRadius="lg"
         borderWidth="1px"
       >
-        <Center fontFamily="work-sans"  color='black' >Talk a Tive</Center>
+        <Center fontSize="4xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Center>
       </Box>
-      <Box></Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </Container>
   );
-};
+}
 
-export default HomePage;
+export default Homepage;
